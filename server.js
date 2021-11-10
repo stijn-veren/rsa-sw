8d6f38a0098d8fb38bfb3d62477317fc570ed29f
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-10 20:02:59 +0200

    Add working server side styled components

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import React from 'react'
+import { ServerStyleSheet } from 'styled-components'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import path from 'path'
@@ -11,10 +12,14 @@ const app = express()
 app.use(express.static('./build', { index: false }))
 
 app.get('/*', (req, res) => {
+  const sheet = new ServerStyleSheet()
+
   const reactApp = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    )
   )
 
   const templateFile = path.resolve('./build/index.html')
@@ -26,6 +31,7 @@ app.get('/*', (req, res) => {
 
     return res.send(
       data.replace('<div id="root></div>', `<div id="root">${reactApp}</div>`)
+        .replace('{{ styles }}', sheet.getStyleTags())
     )
   })
 })
