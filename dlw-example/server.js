9a0ec5905df007e2cf7a0beee6f5676a222d5d15
Kostiantyn Verenych <kverenych@atlassian.com>
2021-12-06 04:22:34 +0200

    dlw-example: Add rendering server-side API data

--- dlw-example/server.js
+++ dlw-example/server.js
@@ -6,6 +6,7 @@ import { StaticRouter } from 'react-router-dom'
 import path from 'path'
 import fs from 'fs'
 import App from './src/App'
+import { InitialDataContext } from './InitialDataContext'
 
 global.window = {}
 
@@ -24,15 +25,31 @@ app.get('/api/articles', (req, res) => {
   res.json(loadedArticles)
 })
 
-app.get('/*', (req, res) => {
+app.get('/*', async (req, res) => {
   const sheet = new ServerStyleSheet()
 
-  const reactApp = renderToString(
+  const contextObj = { _isServerSide: true, _requests: [], _data: {} }
+
+  renderToString(
     sheet.collectStyles(
+      <InitialDataContext.Provider value={contextObj}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </InitialDataContext.Provider>
+    )
+  )
+
+  await Promise.all(contextObj._requests)
+  contextObj._isServerSide = false
+  delete contextObj._requests
+
+  const reactApp = renderToString(
+    <InitialDataContext.Provider value={contextObj}>
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
-    )
+    </InitialDataContext.Provider>
   )
 
   const templateFile = path.resolve('./build/index.html')
@@ -41,14 +58,12 @@ app.get('/*', (req, res) => {
       return res.status(500).send(err)
     }
 
-    const loadedArticles = articles
-
     return res.send(
       data
         .replace(
           '<div id="root"></div>',
-          `<script>window.preloadedArticles = ${JSON.stringify(
-            loadedArticles
+          `<script>window.preloadedData = ${JSON.stringify(
+            contextObj
           )};</script><div id="root">${reactApp}</div>`
         )
         .replace('{{ styles }}', sheet.getStyleTags())
